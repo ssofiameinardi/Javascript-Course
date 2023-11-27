5b0cc040fde3ef0c3b7588113a3937d5d25cccc7
ssofiameinardi <soofiameinardi@gmail.com>
2023-11-27 12:12:35 -0300

    .

---
+++ clase02_nodejs_chance/app.js
@@ -0,0 +1,5 @@
+const {sumar, multiplicar}  = require('./calculadora');
+
+let resultado = sumar(23,7);
+
+console.log(resultado);
---
+++ clase02_nodejs_chance/calculadora.js
@@ -0,0 +1,9 @@
+const sumar = (num1, num2) =>{
+    return num1 + num2;
+}
+
+const multiplicar = (num1, num2) =>{
+    return num1 * num2;
+}
+
+module.exports = {sumar, multiplicar}
