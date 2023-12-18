26e823f365d83e4045c911afb9398c3c2d7d221d
ssofiameinardi <soofiameinardi@gmail.com>
2023-12-18 16:38:18 -0300

    .

---
+++ clase04/api_libros/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+
+const librosRouter = require('./routes/libros');
+const errorHandler = require('./middlewares/errorHandler');
+
+app.use('/libros', librosRouter);
+app.use(errorHandler);
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado en el puerto 3000.');
+});
